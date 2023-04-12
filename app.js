46b9203a818945bed2084e6b17de64ebb3e02f84
nolizwe <nolizwemasondo@gmail.com>
2023-04-12 13:04:21 +0200

    changed whole css

--- app.js
+++ app.js
@@ -1,12 +1,76 @@
-function displayDate() {
-  let date = document.querySelector("#current-time");
-  date.innerHTML = `${day} ${time.getHours()}:${time.getMinutes()}`;
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#search-text-input");
+  cityElement.innerHTML = cityInput.value;
+
+  let h2 = document.querySelector("h2");
+  if (cityInput.value) {
+    h2.innerHTML = `Searching for ${cityInput.value}...`;
+  } else {
+    h2.innerHTML = null;
+    alert("Please enter City");
+  }
+
+  let apiKey = "311f1f45fee82242ab4086372ab360f5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
-let time = new Date();
 
-let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+function searchPosition(position) {
+  let apiKey = "ad35702ba0f5fcbdc3844584b48f901d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+function getCurrentPosition(event) {
+  console.log("hd");
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchPosition);
+}
+let currentPositionButton = document.querySelector("#current-position-button");
+currentPositionButton.addEventListener("click", getCurrentPosition);
 
-let day = days[time.getDay()];
+function showTemperature(response) {
+  console.log(response.data.main);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let description = document.querySelector("#temperature-description");
+  temperatureElement.innerHTML = `${temperature}ºC`;
+  description.innerHTML = response.data.weather[0].description;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+let now = new Date();
+
+let h4 = document.querySelector(".info");
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
 
 let months = [
   "January",
@@ -22,30 +86,6 @@ let months = [
   "November",
   "December"
 ];
+let month = months[now.getMonth()];
 
-let month = months[time.getMonth()];
-
-displayDate();
-
-function showCity(event) {
-  event.preventDefault();
-  let citySearch = document.querySelector("#find-city");
-  let city = document.querySelector(".city");
-  city.innerHTML = `${citySearch.value}`;
-}
-
-let form = document.querySelector("form");
-form.addEventListener("submit", showCity);
-
-function convertTemp(event) {
-  event.preventDefault();
-}
-
-function forFahrenheit() {
-  let fahrenheit = document.querySelector("button.temp-toggle ");
-  fahrenheit.innerHTML = "°C";
-}
-
-function forCelsius() {
-  let celsius = document.querySelector("button.temp-toggle ");
-}
+h4.innerHTML = `${day}, ${month} ${date}, ${year}. ${hours}:${minutes}`;
